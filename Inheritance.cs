46281418074d7867afd608f68a81e49b8b39aecb
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-04-30 20:57:46 +0530

    [Khamisha]Add:Create Program with inheritance Example

---
+++ Inheritance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3A_Assignment
+{
+    public class Inheritance1
+    {
+        public void ParentClass()
+        {
+            Console.WriteLine("This is parent class");
+        }
+    }
+    public class ChildClass : Inheritance1
+    {
+        public void Parent2()
+        {
+            Console.WriteLine("This is derived Class");
+        }
+    }
+}
+
+
--- Program.cs
+++ Program.cs
@@ -6,8 +6,14 @@ namespace Day3A_Assignment
     {
         public static void Main(string[] args)
         {
-            ClassAndObject obj = new ClassAndObject();
-            obj.Display();
+            //ClassAndObject obj = new ClassAndObject();
+            //obj.Display();
+
+            ChildClass obj = new ChildClass();
+            obj.ParentClass();
+            obj.Parent2();
+
+
             Console.ReadKey();
         }
     }
