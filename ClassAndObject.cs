cdf77675a89e87b3bec084e2f56c873f10e85d1f
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-04-30 20:29:45 +0530

    [Khamisha]Add:Create Class and object

---
+++ ClassAndObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3A_Assignment
+{
+    public class ClassAndObject
+    {
+        public void Display()
+        {
+            Console.WriteLine("This is a class and its fields");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using _3A_Assignment;
+
+namespace Day3A_Assignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ClassAndObject obj = new ClassAndObject();
+            obj.Display();
+            Console.ReadKey();
+        }
+    }
+}
