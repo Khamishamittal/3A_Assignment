0fc7d7c3621a4e0e1470a45a2b0199106cfbc8c7
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-05-01 19:59:50 +0530

    [Khamisha]Add:Create Program using Encapsulation

---
+++ Encapsulation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3A_Assignment
+{
+    public class Encapsulation
+    {
+        private string StudentName;
+        private int StudentRollNo;
+
+        public string Name
+        {
+            get
+            {
+                return StudentName;
+            }
+
+            set
+            {
+                StudentName = value;
+            }
+        }
+
+        public int RollNo
+        {
+            get
+            {
+                return StudentRollNo;
+            }
+            set
+            {
+                StudentRollNo = value;
+            }
+        }
+
+    }
+
+}
+    
+
--- Program.cs
+++ Program.cs
@@ -14,20 +14,23 @@ namespace Day3A_Assignment
             //obj.Parent2();
 
             //overloading
-            Polymorphism obj = new Polymorphism();
-            Console.WriteLine(obj.Add(8, 9));
-            Console.WriteLine(obj.Add(6, 5, 3));
-            Console.WriteLine(obj.Add(6.8, 9.7, 6.4));
+            //Polymorphism obj = new Polymorphism();
+            //Console.WriteLine(obj.Add(8, 9));
+            //Console.WriteLine(obj.Add(6, 5, 3));
+            //Console.WriteLine(obj.Add(6.8, 9.7, 6.4));
 
             //overriding
-            Car obj1 = new Car();
-            obj1.honk();
+            //Car obj1 = new Car();
+            //obj1.honk();
+            //Bus obj2 = new Bus();
+            //obj2.honk();
+            //Console.ReadKey();
 
-            Bus obj2 = new Bus();   
-            obj2.honk();
-
-
-            Console.ReadKey();
+            Encapsulation obj = new Encapsulation();
+            obj.Name = "Teeeeeeeeeeeeeeeeeeeeeeeennnnnnnnnnnaaaaaaaaaaaaaa";
+            obj.RollNo = 34;
+            Console.WriteLine(obj.RollNo);
+            Console.WriteLine(obj.Name);
         }
     }
 }
